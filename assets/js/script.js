fd3313321bd03f90ab05b8b5ea14cc9ced4614d2
Banele Victor Mayisela <41682195+SudoCyberZeus@users.noreply.github.com>
2019-06-04 10:50:56 +0200

    Official Launch
    Publishing the finished product.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,11 @@
-class Alcohol {
-    constructor(name, size, kind, price) {
-        this.name = name;
-        this.size = size;
-        this.kind = kind;
-        this.price = price;
-    }
-    displayAlcohol() {
-        return `
-            <div class="pa-ad">
-                <b>${this.name}</b> <br>
-                ${this.kind} <br>
-                ${this.size} <br>
-                ${this.price}
-            </div>
-        `
-    }
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
 }
 
-var myAlcohol = document.getElementById('myAlcohol');
-
-var xhttml = new XMLHttpRequest();
-
-xhttml.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-        var responce = JSON.parse(xhttml.responseText);
-        
-        for(let i = 0;i < responce.length; i++){
-            const alcohol = new Alcohol(responce[i].name, responce[i].size, responce[i].kind, responce[i].price);
-
-            myAlcohol.innerHTML += alcohol.displayAlcohol();
-        }
-    }
-};
-xhttml.open("GET", "alcohol.json", true);
-xhttml.send();
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("paContent").style.display = "grid";
+    document.getElementById("paContent").style.animation = "fadeIn 6s";
+}
