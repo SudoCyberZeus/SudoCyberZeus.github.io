0b5594bd15d4c8c048c00f000fc78276564cbebd
Banele Victor Mayisela <41682195+SudoCyberZeus@users.noreply.github.com>
2021-10-04 20:45:34 +0200

    Pro
    PRJ

--- README.md
+++
@@ -1,4 +0,0 @@
-# SudoCyberZeus.github.io
-Personal Website - Online Resume 
-
-For some crazy reason, this is not working. Nxi
--- banele-victor-mayisela/assets/js/javascript.js
+++
@@ -1,8 +0,0 @@
-function paPageOpen() {
-    var pa_cover = document.getElementById("paCover");
-    var pa_content = document.getElementById("paContent");
-    
-    pa_cover.className = "pa-page-open";
-
-    setTimeout(function(){ pa_content.className += " pa-content-show"; }, 2000);
-}
